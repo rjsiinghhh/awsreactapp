2349b5b02f03b51b1626f3be35b81a0738e46f24
rjsiinghhh <>
2020-12-16 12:15:03 -0800

    11

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
@@ -41,32 +40,63 @@ function App() {
     fetchNotes();
   }
 
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
+  }
+
+
+  async function createNote() {
+  if (!formData.name || !formData.description) return;
+  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+  if (formData.image) {
+    const image = await Storage.get(formData.image);
+    formData.image = image;
+  }
+  setNotes([ ...notes, formData ]);
+  setFormData(initialFormState);
+}
+
+
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+    <AmplifySignOut />
+    <img src="https://i.imgur.com/A0PFbLn.png" /> <br />
+
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Note"
         value={formData.name}
       />
       <input
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
+        placeholder="Description"
         value={formData.description}
       />
       <button onClick={createNote}>Create Note</button>
       <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-            </div>
-          ))
-        }
+      {
+  notes.map(note => (
+    <div key={note.id || note.name}>
+      <h2>{note.name}</h2>
+      <p>{note.description}</p>
+      <button onClick={() => deleteNote(note)}>Delete note</button>
+      {
+        note.image && <img src={note.image} style={{width: 400}} alt="whaaat" />
+      }
+    </div>
+  ))
+}
       </div>
-      <AmplifySignOut />
+
     </div>
   );
 }
